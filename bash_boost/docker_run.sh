5f1d206fc0d0effb91039bb94eb30805e1720df7
andy-duncan <andy.duncan@tesglobal.com>
2019-09-06 16:32:28 +0200

    upgrade to bash 4 to allow no sort on the completion. add a connection check

--- bash_boost/docker_run.sh
+++ bash_boost/docker_run.sh
@@ -3,8 +3,20 @@
 
 bearer="Authorization: Bearer $DOCKER_RUN_KUBERNAUT_BEARER_TOKEN"
 
+_test_connection()
+{
+  connection=$(curl --write-out %{http_code} --max-time 5 -s -H "$bearer" https://kubernaut.tescloud.com/api/services?limit=1)
+
+  if [[ "$connection" = "000" ]]; then
+    echo "connection timed out: please ensure you're connected to the vpn and try again"
+    return
+  fi
+}
+
 _dr_completions()
 {
+  _test_connection
+
   if [ ${COMP_CWORD} -eq 1 ]; then
     # Kube services to get services
     services=`curl -s -H "$bearer" https://kubernaut.tescloud.com/api/services?limit=1000000 | jq '.items[].name'`
@@ -14,13 +26,16 @@ _dr_completions()
 
   if [ ${COMP_CWORD} -eq 2 ]; then
     # Kube releases to get versions.
-    releases=`curl -s -H "$bearer" "https://kubernaut.tescloud.com/api/releases?limit=500&offset=0&service=${COMP_WORDS[1]}&version=value:${COMP_WORDS[2]},not:false,exact:false&order=desc&sort=created" | jq '.items[].version'`
+    releases=`curl -s -H "$bearer" "https://kubernaut.tescloud.com/api/releases?limit=500&offset=0&service=${COMP_WORDS[1]}&version=value:${COMP_WORDS[2]},not:false,exact:false&order=desc&sort=created" | jq -r '.items[].version' `
 
-    COMPREPLY=($(compgen -W "$releases" ""))
+    COMPREPLY=(${releases[@]})
   fi
 }
 
-docker_run() {
+docker_run()
+{
+  _test_connection
+
   # Kube get the specific release to get the id
   id=`curl -s -H "$bearer" "https://kubernaut.tescloud.com/api/releases?limit=50&offset=0&service=$1&version=$2" | jq  -r '.items[].id'`
 
@@ -34,4 +49,4 @@ docker_run() {
 
 alias dr=docker_run
 
-complete -F _dr_completions dr
+complete -o nosort -F _dr_completions dr
